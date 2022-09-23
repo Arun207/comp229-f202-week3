b14f7ec9c585eeb203486c485aae774709c6f8ef
Arun207 <arunparambath0236@gmail.com>
2022-09-23 19:03:17 -0400

    initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,8 @@
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+    function start(){
+        console.log("App started...");
+    }
+
+    window.addEventListener("load",start);
+})();
