2ee77418e6cc56c42f51bce0dd3316d837d3b797
Arun207 <arunparambath0236@gmail.com>
2022-10-25 21:47:37 -0400

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
 module.exports = 
 {
-    "URI": "mongodb://127.0.0.1/book_store"
+   // "URI": "mongodb://127.0.0.1/book_store"
+
+   "URI": "mongodb+srv://Arun:HESltChhuUcqrXvF@cluster0.lpz0zxu.mongodb.net/book_store?retryWrites=true&w=majority"
+
 }
